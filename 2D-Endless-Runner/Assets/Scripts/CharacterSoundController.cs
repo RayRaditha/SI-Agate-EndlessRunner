f8910a8c745dffb832994dbeb28014a4d8b9b8e6
RayRaditha <puturadithaa@gmail.com>
2021-09-15 18:27:22 +0700

    Tugas Agate Game Endless Runner

---
+++ 2D-Endless-Runner/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    private AudioSource audioPlayer;
+
+    public AudioClip scoreHighlight;
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+}
